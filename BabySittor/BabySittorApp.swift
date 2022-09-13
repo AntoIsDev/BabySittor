4c9066b01656692462f9a11c8772add309a815de
Antonin De Almeida <15748781+aFlyerz@users.noreply.github.com>
2022-09-13 18:24:38 +0200

    Initializing project

---
+++ BabySittor/BabySittorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySittorApp.swift
+//  BabySittor
+//
+//  Created by Antonin De Almeida on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BabySittorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
